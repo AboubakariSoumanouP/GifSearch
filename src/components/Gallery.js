23e41b6ef557c690dd1d110ecbd94eac87484b6b
AboubakariSoumanouP <Soumabakari@gmail.com>
2020-05-09 17:45:36 -0400

    complete searchbox

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,23 @@
 import React from "react";
+import SearchBox from "./SearchBox";
 
-class App extends React.Component {
-  
+class Gallery extends React.Component {
+  fetchGif = (search) => {
+    if (search.trim().length > 0) {
+      fetch(
+        `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=X8oKb5Tpms58uaWXxfKyqfoD9A9PYL9A`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+
+  handleClick = (url) => {
+    this.setState({ imageUrl: url });
+  };
+
   render() {
 
     return (
@@ -9,19 +25,10 @@ class App extends React.Component {
         <div className="header">
           <h2 className="title">Gif Search</h2>
         </div>
-
-        <div className="input-container">
-          <input
-            id="gif-search-input"
-          />
-          <button id="gif-search-submit" onClick={this.fetchGif}>
-            Search
-          </button>
-        </div>
+        <SearchBox fetchGif={this.fetchGif} />
       </div>
     );
   }
 }
 
-export default App;
-
+export default Gallery;
