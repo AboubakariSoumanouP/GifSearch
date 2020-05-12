2c814bb6b1f2abadcaf4304e1ce3f7402b73bee4
AboubakariSoumanouP <Soumabakari@gmail.com>
2020-05-12 09:28:31 -0400

    Updates

---
+++ precache-manifest.b4619686fef351156bb4788cecfb291f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ad5d3133387440a16bbd6b6c65ce2e",
+    "url": "/GifSearch/index.html"
+  },
+  {
+    "revision": "038d6ad9cba7483f477f",
+    "url": "/GifSearch/static/css/main.9591f43d.chunk.css"
+  },
+  {
+    "revision": "93f69d85d237a7656439",
+    "url": "/GifSearch/static/js/2.5cdaa139.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GifSearch/static/js/2.5cdaa139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038d6ad9cba7483f477f",
+    "url": "/GifSearch/static/js/main.403f4c54.chunk.js"
+  },
+  {
+    "revision": "c6838492027df7b4009c",
+    "url": "/GifSearch/static/js/runtime-main.2001cca5.js"
+  }
+]);
